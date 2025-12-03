e34df9e5d306bd838bbe0b1f9a394d0233f1b1db
harshada2576 <harshada2576@gmail.com>
2025-12-03 18:58:27 +0530

    library system

---
+++ java-project/src/p7/Book.java
@@ -0,0 +1,17 @@
+package p7;
+
+public class Book extends LibraryItem {
+    private int numberOfPages;
+
+    public Book(String title, String author, int numberOfPages) {
+        super(title, author);
+        this.numberOfPages = numberOfPages;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Pages: " +
+                numberOfPages);
+    }
+}
---
+++ java-project/src/p7/DVD.java
@@ -0,0 +1,17 @@
+package p7;
+
+public class DVD extends LibraryItem {
+    private int duration;
+
+    public DVD(String title, String author, int duration) {
+        super(title, author);
+        this.duration = duration;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Duration: " + duration +
+                " minutes");
+    }
+}
